89f545c544344034b9b154c8499364ea7dc78d8f
WinstonJohnNetinim <32879234+WinstonJohnNetinim@users.noreply.github.com>
2017-11-07 16:10:47 -0300

    Atualizacao bugada legal
    progeto feito na aula dia 07/10 - fazer as mudanças certas

--- Assets/Scripts/BampersController.cs
+++ Assets/Scripts/BampersController.cs
@@ -7,7 +7,7 @@ public class BampersController : MonoBehaviour {
 	// Use this for initialization
 	void Start() 
 	{
-		gameObject.SetActive (false);
+		gameObject.SetActive (true);
 	}
 	void Update () {
 		transform.Rotate (30 * Time.deltaTime, 45 * Time.deltaTime, 0);
---
+++ Assets/Scripts/InimigoHunter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class InimigoHunter : MonoBehaviour {
+
+	public float patrolDistance = 30;
+	private GameObject player;
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		agent = GetComponent <NavMeshAgent> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Vector3.Distance (player.transform.position, agent.transform.position) < patrolDistance) {
+			agent.destination = player.transform.position;
+		}
+	}
+}
